6e97e0707234c8ad09d84b8ade13971d249a30f5
Nitin Goel <nitin.goel@kellogg.northwestern.edu>
2021-02-19 18:02:41 -0600

    final commit

--- homework/movies.js
+++ homework/movies.js
@@ -1,43 +1,64 @@
-window.addEventListener('DOMContentLoaded', async function(event) {
+firebase.auth().onAuthStateChanged(async function (user) {
+  if (user) {
+    let currentUserName = firebase.auth().currentUser.displayName
+   // Signout button
+   document.querySelector(".sign-in-or-sign-out").innerHTML = `
+   <p class="font-bold text-xl">Signed in as ${currentUserName}</p>
+   <a href="#" class="sign-out-button text-pink-500 underline">Sign Out</a>
+ `
+  document.querySelector(".sign-out-button").addEventListener("click", function(event){
+    event.preventDefault()
+    firebase.auth().signOut()
+    document.location.href = "movies.html"
+  })
   let db = firebase.firestore()
   let apiKey = 'your TMDB API key'
-  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
+  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=883c2c519468a9f5df526f211693ca9d&language=en-US`)
   let json = await response.json()
   let movies = json.results
   console.log(movies)
-  
   for (let i=0; i<movies.length; i++) {
     let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
+    let docRef = await db.collection('watched').doc(`${movie.id}-${firebase.auth().currentUser.uid}`).get()
     let watchedMovie = docRef.data()
     let opacityClass = ''
     if (watchedMovie) {
       opacityClass = 'opacity-20'
     }
-
     document.querySelector('.movies').insertAdjacentHTML('beforeend', `
       <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
         <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
       </div>
     `)
-
     document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
       event.preventDefault()
       let movieElement = document.querySelector(`.movie-${movie.id}`)
       movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
+      await db.collection('watched').doc(`${movie.id}-${firebase.auth().currentUser.uid}`).set({
+        userID: firebase.auth().currentUser.uid,
+        movieID: movie.id
+      })
     }) 
   }
-})
-
+} else {
+    // Initializes FirebaseUI Auth
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+    // FirebaseUI configuration
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'movies.html'
+    }
+    // Starts FirebaseUI Auth
+    ui.start('.sign-in-or-sign-out', authUIConfig)
+  }})
 // Goal:   Refactor the movies application from last week, so that it supports
 //         user login and each user can have their own watchlist.
-
 // Start:  Your starting point is one possible solution for last week's homework.
-
 // Step 1: Add your Firebase configuration to movies.html, along with the
-//         (provided) script tags for all necessary Firebase services – i.e. Firebase
+//         (provided) script tags for all necessary Firebase services – i.e. Firebase
 //         Auth, Firebase Cloud Firestore, and Firebase UI for Auth; also
 //         add the CSS file for FirebaseUI for Auth.
 // Step 2: Change the main event listener from DOMContentLoaded to 
